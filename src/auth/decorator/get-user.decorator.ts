6998c05de04dd076bc154b0257e33d11161cbeae
a01081440011 <a01081440011@gmail.com>
2022-05-06 20:50:16 +0900

    feat(getUserDecorator): get specific user props

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,8 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/user/schemas/user.schema';
 
 export const GetUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
+  (key: keyof User | undefined, ctx: ExecutionContext) => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
+    if (key) {
+      return request.user[key];
+    }
     return request.user;
   },
 );
